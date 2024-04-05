fcb8d9e1c30df9b55a3a907abf105876d719a7ad
claraharguindey <clarisaharguindey@gmail.com>
2024-04-05 15:49:23 +0200

    add frames

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function () {
   const scrollPosition = window.scrollY;
-  const totalImages = 20;
+  const totalImages = 30;
   const imageIndex =
     (Math.floor(
       (scrollPosition / (document.body.clientHeight - window.innerHeight)) *
